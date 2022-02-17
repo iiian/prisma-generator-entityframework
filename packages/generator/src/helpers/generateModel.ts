ee0c35db39b26dcae4f29f1f9371728735d347e8
Ian Ray <iiian.develops@gmail.com>
2022-02-17 05:27:02 -0800

    feat: introduce multi-field primary-key support

--- packages/generator/src/helpers/generateModel.ts
+++ packages/generator/src/helpers/generateModel.ts
@@ -24,20 +24,6 @@ Handlebars.registerHelper('isTruthy', function (value) {
   return value !== undefined && value !== null;
 });
 
-Handlebars.registerHelper('getDbField', function (field_name: string, model_name: string, raw_schema_lines: string[]) {
-  // required in order to differentiate fields w/ the same name on different models, which _could_ in theory be mapped to different
-  // field names on the db side.
-  const model_line_start = raw_schema_lines.findIndex(line => line.includes(`model ${model_name}`));
-
-  // get the first line on the model that matches the field
-  const line = raw_schema_lines.find((line, index) => new RegExp(`^\\s*${field_name}.*$`).test(line) && index > model_line_start)!;
-
-  // extract the @map("field") annotation, or default to the field_name
-  const db_field_name = /\@map\(\"(?<db_field_name>.*)\"\)/.exec(line)?.groups?.db_field_name ?? field_name;
-
-  return db_field_name;
-});
-
 Handlebars.registerHelper('getDbName', (model: DMMF.Model) => {
   return model.dbName ?? model.name;
 });
@@ -58,6 +44,59 @@ Handlebars.registerHelper('getCSType', (field: DMMF.Field) => {
   }
   return type;
 });
+
+Handlebars.registerHelper('getColumnAnnotation', (field: DMMF.Field, model: DMMF.Model, raw_schema_lines: string[]) => {
+  const order = getOrderIndex(field, model);
+  const field_name = getDbField(field.name, model.name, raw_schema_lines);
+  const is_primary_key = isPrimaryKey(field, model);
+  const key_annotation_prefix = (is_primary_key && 'Key, ') || '';
+  if (order < 0 && !field_name) {
+    return `[${key_annotation_prefix}Column]`;
+  }
+  if (field_name) {
+    return `[${key_annotation_prefix}Column("${field_name}")]`;
+  }
+  const field_name_string = field_name && `"${field_name}", ` || '';
+  return `[Key, Column(${field_name_string}Order=${order})]`;
+});
+
+function isPrimaryKey(field: DMMF.Field, model: DMMF.Model) {
+  if (field.isId) {
+    return true;
+  }
+  if (!model.primaryKey?.fields) {
+    return false;
+  }
+  const index = model.primaryKey!.fields.findIndex(each_primary_key => each_primary_key === field.name);
+  return index >= 0;
+}
+
+function getDbField(field_name: string, model_name: string, raw_schema_lines: string[]) {
+  // required in order to differentiate fields w/ the same name on different models, which _could_ in theory be mapped to different
+  // field names on the db side.
+  const model_line_start = raw_schema_lines.findIndex(line => line.includes(`model ${model_name}`));
+
+  // get the first line on the model that matches the field
+  const line = raw_schema_lines.find((line, index) => new RegExp(`^\\s*${field_name}.*$`).test(line) && index > model_line_start)!;
+
+  // extract the @map("field") annotation, or default to the field_name
+  const db_field_name = /\@map\(\"(?<db_field_name>.*)\"\)/.exec(line)?.groups?.db_field_name ?? null;
+
+  return db_field_name;
+}
+
+function getOrderIndex(field: DMMF.Field, model: DMMF.Model): number {
+  if (!model.primaryKey?.fields) {
+    return -1;
+  }
+  const { fields } = model.primaryKey;
+  const index = fields.findIndex(each_primary_key => each_primary_key === field.name);
+  if (index < 0) {
+    return -1;
+  }
+  // this is dependent on us always outputting the "ColumnId" in the [Column] annotation
+  return index;
+}
 
 export function generateModel({ model, namespace, schema_file_path }: GenerateModelParams): string {
   const template_text = readFileSync(join(__dirname, '..', 'templates', 'Model.cs.hbs')).toString();
